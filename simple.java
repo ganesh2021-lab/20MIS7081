72601d51d43303289492e09a0af01d6ecf3f83da
ganesh <ganesh.20mis7081@gmail.com>
2021-03-10 16:56:27 +0530

    changes made to aprile release

--- simple.java
+++ simple.java
@@ -1,5 +1,8 @@
 class Simple{  
-    public static void main(String args[]){  
+    public static void main(String args[])
+    int a.b.c;{  
      System.out.println("Hello  Java is a good one");  
-    }  
+    } System.out.println("Hello ");
+} System.out.println("This is my April-release code");
+  
 }  
